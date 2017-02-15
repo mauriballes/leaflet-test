4f97362c941ad047eb200059a0f47afb3edf448b
Mauricio Ballesteros <mballes95@gmail.com>
2017-02-15 14:02:13 -0400

    Trying to integrate angular

--- app/app.js
+++ app/app.js
@@ -1,4 +1,16 @@
 (function () {
-    var name = "Mauricio";
-    console.log(name);
+    /*
+    * Testing maps
+    */
+
+    var map = L.map('map').setView([-17.789167, -63.1975], 13);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([-17.789167, -63.1975]).addTo(map)
+        .bindPopup('Popup de un punto.<br>Facil de personalizar.')
+        .openPopup();
+
 })();
